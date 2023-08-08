387b3e9adcc017671291c0fca879185b354a893e
ola567 <aleksandra.hein@o2.pl>
2023-08-08 16:52:40 +0200

    shell context

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-from app import app
+from app import app, db
+from app.models import User, Post
 
 
+@app.shell_context_processors
+def make_shell_contex():
+    return {'db': db, 'User': User, 'Post': Post}
