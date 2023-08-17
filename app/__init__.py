4c9bd31a3e05a366cd40ce802fa1a645e9035c66
ola567 <aleksandra.hein@o2.pl>
2023-08-18 00:10:59 +0200

    forgot password

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,6 +12,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 from app import routes, models, errors
 import os
