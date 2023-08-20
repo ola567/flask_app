7e16c9f1c7da5a03f749da16c61c0694e9cddfea
ola567 <aleksandra.hein@o2.pl>
2023-08-20 22:20:14 +0200

    bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,6 +14,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app=app)
 
 from app import routes, models, errors
 import os
